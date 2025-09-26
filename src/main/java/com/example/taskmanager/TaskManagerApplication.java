604c536e62ca86cab25ee9dbb182c8ccc5dcd852
Michal Urbanczyk <michaurbanczyk@gmail.com>
2025-09-26 15:43:54 +0200

    User Model and User Controller

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @Configuration, @EnableAutoConfiguration, @ComponentScan
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
     }
-
 }
